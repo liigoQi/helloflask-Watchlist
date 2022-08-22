58004d3aa5a33b20274238f318f17e37d04122bc
KGQualityEvaluation <1051546251@qq.com>
2022-08-22 19:02:00 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from importlib.util import resolve_name
-from operator import is_
 from flask import Flask, url_for, render_template
 # 注意 用户输入的数据会包含恶意代码，所以不能直接作为响应返回，需要使用 MarkupSafe（Flask 的依赖之一）提供的 escape() 函数对 name 变量进行转义处理，比如把 < 转换成 &lt;。这样在返回响应时浏览器就不会把它们当做代码执行。
 from markupsafe import escape 
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, flash
 import os 
 import sys 
 import click 
 
 app = Flask(__name__)
+
+# flash() 函数在内部会把消息存储到 Flask 提供的 session 对象里。session 用来在请求间存储数据，它会把数据签名后存储到浏览器的 Cookie 中，所以我们需要设置签名所需的密钥
+app.config['SECRET_KEY'] = 'dev'
 
 # 对于这个变量值，不同的 DBMS 有不同的格式，对于 SQLite 来说，这个值的格式如下：
 # sqlite:////数据库文件的绝对地址
@@ -85,8 +87,24 @@ def inject_user():
 
 # register a view function
 @app.route('/home')
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+# 两种方法的请求有不同的处理逻辑：对于 GET 请求，返回渲染后的页面；对于 POST 请求，则获取提交的表单数据并保存。
 def index():
+    # Flask 会在请求触发后把请求信息放到 request 对象里
+    # 它包含请求相关的所有信息，比如请求的路径（request.path）、请求的方法（request.method）、表单数据（request.form）、查询字符串（request.args）等等。
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            # 在用户执行某些动作后，我们通常在页面上显示一个提示消息。最简单的实现就是在视图函数里定义一个包含消息内容的变量，传入模板，然后在模板里渲染显示它。因为这个需求很常用，Flask 内置了相关的函数。其中 flash() 函数用来在视图函数里向模板传递提示消息，get_flashed_messages() 函数则用来在模板中获取提示消息。
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+    
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
@@ -108,3 +126,28 @@ def test_url_for():
 def page_not_found(e): # 接受异常对象作为参数
     return render_template('404.html'), 404 # 返回模板和状态码
     # 普通的视图函数之所以不用写出状态码，是因为默认会使用 200 状态码，表示成功。
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    # get_or_404(): 它会返回对应主键的记录，如果没有找到，则返回 404 错误响应。
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+        movie.title = title 
+        movie.year = year 
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('edit', movie_id=movie_id))
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item delete.')
+    return redirect(url_for('index'))
