48c13fa564cd7ac83de0055d3d992e51e880231d
KGQualityEvaluation <1051546251@qq.com>
2022-08-22 16:56:11 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 # 注意 用户输入的数据会包含恶意代码，所以不能直接作为响应返回，需要使用 MarkupSafe（Flask 的依赖之一）提供的 escape() 函数对 name 变量进行转义处理，比如把 < 转换成 &lt;。这样在返回响应时浏览器就不会把它们当做代码执行。
 from markupsafe import escape 
+
+name = 'Liigo'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 app = Flask(__name__)
 
@@ -8,7 +22,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/')
 def hello():
-    return f'<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/usr/<name>')
 def user_page(name):
