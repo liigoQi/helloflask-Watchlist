4b1549a8e43aed57cc5eb0cb267491f3f7bca3fc
KGQualityEvaluation <1051546251@qq.com>
2022-08-23 11:15:33 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -8,7 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+# 读取系统环境变量 SECRET_KEY 的值，如果没有获取到，则使用 dev
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -16,7 +17,7 @@ if WIN:
 else:
     prefix = 'sqlite:////'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 # 关闭对模型修改的监控
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
